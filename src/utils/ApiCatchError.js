0d210abc74d418ec7f763a5adaa24e5d8a0ee7e3
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-05 23:58:56 +0530

    Api catch error response

---
+++ src/utils/ApiCatchError.js
@@ -0,0 +1,9 @@
+
+// catch block error handler function
+export const ApiCatchError = (res, message, error, statusCode = 500) => {
+    return res.status(statusCode).json({
+        success: false,
+        message: message || 'Internal Server Error',
+        error: error.stack
+    });
+};
